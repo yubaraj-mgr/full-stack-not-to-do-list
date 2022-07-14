0b049e0a2bdab38312ee38bad2f75b4bf282369a
Yubaraj Magar <yubaraj.100mgr@gmail.com>
2022-07-14 14:16:26 +1000

    final

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,10 @@ app.use("/api/v1/task", taskRouter);
 
 // static content serve
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/FRONTEND/build")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.use("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/FRONTEND/build/index.html"));
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
 
 app.use((error, req, res, next) => {
